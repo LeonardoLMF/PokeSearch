04f07b7ae1866d99ac5f9a8f6bd59aa15b778f29
Leonardo Luiz <leonardo.lmf27@gmail.com>
2025-02-18 00:03:49 -0300

    start commit

---
+++ src/main/java/br/com/pokesearchleo/pokesearch/PokesearchApplication.java
@@ -0,0 +1,13 @@
+package br.com.pokesearchleo.pokesearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokesearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokesearchApplication.class, args);
+	}
+
+}
